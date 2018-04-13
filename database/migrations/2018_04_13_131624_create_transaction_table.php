bc7b185cbe348bb1058fb607f8bc03a909d1af8c
Ken Vilar <kenvilar@gmail.com>
2018-04-13 21:18:07 +0800

    Add new transctions column

--- database/migrations/2018_04_13_131624_create_transaction_table.php
+++ database/migrations/2018_04_13_131624_create_transaction_table.php
@@ -15,6 +15,7 @@ class CreateTransactionTable extends Migration
     {
         Schema::create('tranactions', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('description');
             $table->timestamps();
         });
     }
