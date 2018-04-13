e6bb55b45fd2c0d2a2bdbfc515d607f96ab288eb
Ken Vilar <kenvilar@gmail.com>
2018-04-13 21:49:24 +0800

    Laravel TestCase base class with exception handling helper methods

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,46 @@
 
 namespace Tests;
 
+use App\Exceptions\Handler;
+use Exception;
+use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->disableExceptionHandling();
+    }
+
+    protected function disableExceptionHandling()
+    {
+        $this->oldExceptionHandler = app()->make(ExceptionHandler::class);
+        app()->instance(ExceptionHandler::class, new PassThroughHandler);
+    }
+
+    protected function withExceptionHandling()
+    {
+        app()->instance(ExceptionHandler::class, $this->oldExceptionHandler);
+        return $this;
+    }
+}
+
+class PassThroughHandler extends Handler
+{
+    public function __construct()
+    {
+    }
+
+    public function report(Exception $e)
+    {
+    }
+
+    public function render($request, Exception $e)
+    {
+        throw $e;
+    }
 }
