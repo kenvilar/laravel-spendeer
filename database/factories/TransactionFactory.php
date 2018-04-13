29e3be4c28c2f03c150b9a630d9aceaa9aea48fb
Ken Vilar <kenvilar@gmail.com>
2018-04-13 22:29:02 +0800

    Add category_id column in TransactionFactory

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Category;
 use \App\Transaction;
 use Faker\Generator as Faker;
 
 $factory->define(Transaction::class, function (Faker $faker) {
     return [
         'description' => $faker->sentence(2),
+        'category_id' => function () {
+            return factory(Category::class)->create()->id;
+        }
     ];
 });
