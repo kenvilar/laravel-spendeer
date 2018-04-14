70005339fb1871d07e6b4fe61c7fd66d37cda7a4
Ken Vilar <kenvilar@gmail.com>
2018-04-14 13:10:38 +0800

    Make it one line and easy to understand

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -15,11 +15,7 @@ class TransactionsController extends Controller
      */
     public function index(Category $category)
     {
-        if ($category->exists) {
-            $transactions = Transaction::query()->where('category_id', $category->id)->get();
-        } else {
-            $transactions = Transaction::all();
-        }
+        $transactions = Transaction::byCategory($category)->get();
 
         return view('transactions.index')->with(['transactions' => $transactions]);
     }
