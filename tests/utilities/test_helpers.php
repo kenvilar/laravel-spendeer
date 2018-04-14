4e486fd11730a11d744b320aa7234a8eb597aa72
Ken Vilar <kenvilar@gmail.com>
2018-04-14 13:15:12 +0800

    Create makeFactory test helper

--- tests/utilities/test_helpers.php
+++ tests/utilities/test_helpers.php
@@ -4,3 +4,8 @@ function createFactory($class, $attributes = [], $times = null)
 {
     return factory($class, $times)->create($attributes);
 }
+
+function makeFactory($class, $attributes = [], $times = null)
+{
+    return factory($class, $times)->make($attributes);
+}
