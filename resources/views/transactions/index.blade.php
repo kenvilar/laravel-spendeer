3acb11828546d25dfb0d96ba65c9c24d6770525d
Ken Vilar <kenvilar@gmail.com>
2018-04-15 16:05:05 +0800

    Create dynamic links;  Improve transactions.index

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -2,28 +2,32 @@
 
 @section('content')
     <div class="container">
-        <div class="table-responsive">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Description</th>
-                        <th>Category</th>
-                        <th>Amount</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($transactions as $transaction)
+        <div class="card">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
                         <tr>
-                            <td>{{ $transaction->created_at->format('m/d/Y') }}</td>
-                            <td>{{ $transaction->description }}</td>
-                            <td>{{ $transaction->category->name }}</td>
-                            <td>{{ $transaction->amount
-                             }}</td>
+                            <th>Date</th>
+                            <th>Description</th>
+                            <th>Category</th>
+                            <th>Amount</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                        </thead>
+                        <tbody>
+                        @foreach($transactions as $transaction)
+                            <tr>
+                                <td>{{ $transaction->created_at->format('m/d/Y') }}</td>
+                                <td>{{ $transaction->description }}</td>
+                                <td>{{ $transaction->category->name }}</td>
+                                <td>{{ $transaction->amount
+                             }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
