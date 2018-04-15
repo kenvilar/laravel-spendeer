85d804ffb2783ccc3f244e70a4edf42fb2a0fe48
Ken Vilar <kenvilar@gmail.com>
2018-04-15 21:56:13 +0800

    Improve UpdateTransactionsTest

--- tests/Feature/UpdateTransactionsTest.php
+++ tests/Feature/UpdateTransactionsTest.php
@@ -26,4 +26,46 @@ class UpdateTransactionsTest extends TestCase
         $this->get("/transactions")
             ->assertSee($newTransaction->description);
     }
+
+    /**
+     * @test
+     */
+    public function it_cannot_update_a_transaction_without_description()
+    {
+        $this->updateTransaction(['description' => null])
+            ->assertSessionHasErrors('description');
+    }
+
+    /**
+     * @test
+     */
+    public function it_cannot_update_a_transaction_without_category()
+    {
+        $this->updateTransaction(['category_id' => null])->assertSessionHasErrors('category_id');
+    }
+
+    /**
+     * @test
+     */
+    public function it_cannot_update_a_transaction_without_valid_amount()
+    {
+        $this->updateTransaction(['amount' => 'thisIsString'])->assertSessionHasErrors('amount');
+    }
+
+    /**
+     * @test
+     */
+    public function it_cannot_update_a_transaction_without_amount()
+    {
+        $this->updateTransaction(['amount' => null])->assertSessionHasErrors('amount');
+    }
+
+    protected function updateTransaction($overrides = [])
+    {
+        $transaction = $this->createFactory(Transaction::class);
+        $newTransaction = $this->makeFactory(Transaction::class, $overrides);
+
+        return $this->withExceptionHandling()
+            ->patch("/transactions/{$transaction->id}", $newTransaction->toArray());
+    }
 }
