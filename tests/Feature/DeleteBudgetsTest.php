935e031633c108020650a4947677cd3a9b74d315
Ken Vilar <kenvilar@gmail.com>
2018-04-17 16:06:06 +0800

    Make DeleteBudgetsTest class

---
+++ tests/Feature/DeleteBudgetsTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class DeleteBudgetsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     */
+    public function it_()
+    {
+        $this->assertTrue(true);
+    }
+}
