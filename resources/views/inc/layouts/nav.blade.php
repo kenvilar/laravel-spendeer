2ee405d7fd42cdf5bdef50a74305cf03072c492f
Ken Vilar <kenvilar@gmail.com>
2018-04-17 14:13:58 +0800

    Organize nav menus

--- resources/views/inc/layouts/nav.blade.php
+++ resources/views/inc/layouts/nav.blade.php
@@ -1,8 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
-        </a>
+        <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -12,20 +10,25 @@
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
                 @auth
-                    <li><a class="nav-link" href="{{ url('/transactions') }}">All Transaction</a></li>
-                    <li><a class="nav-link" href="{{ url('/transactions/create') }}">New Transaction</a></li>
                     <li class="dropdown">
                         <a class="dropdown-toggle nav-link" href="#" data-toggle="dropdown" role="button"
-                           aria-expanded="false">
-                            Categories
-                        </a>
+                           aria-expanded="false">Transactions</a>
                         <ul id="dropdown" class="dropdown-menu" role="menu">
+                            <li><a class="nav-link" href="{{ url('/transactions') }}">All Transactions</a></li>
+                            <li role="separator" class="dropdown-divider"></li>
+                            <li><a class="nav-link" href="{{ url('/transactions/create') }}">New Transaction</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle nav-link" href="#" data-toggle="dropdown" role="button"
+                           aria-expanded="false">Categories</a>
+                        <ul id="dropdown" class="dropdown-menu" role="menu">
+                            <li><a class="nav-link" href="{{ url('/categories') }}">Manage Categories</a></li>
+                            <li><a class="nav-link" href="{{ url('/categories/create') }}">New Category</a></li>
+                            <li role="separator" class="dropdown-divider"></li>
                             @foreach(\App\Category::all() as $category)
-                                <li>
-                                    <a class="nav-link" href="{{ url('/transactions/' . $category->slug) }}">
-                                        {{ $category->name }}
-                                    </a>
-                                </li>
+                                <li><a class="nav-link" href="{{ url('/transactions/' . $category->slug) }}">
+                                        {{ $category->name }}</a></li>
                             @endforeach
                         </ul>
                     </li>
@@ -42,15 +45,12 @@
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
+                            {{ Auth::user()->name }} <span class="caret"></span></a>
 
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
+                               onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}</a>
 
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                 @csrf
